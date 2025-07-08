e53d22345324395d4012fece10b4df763bf5154e
Michael <mkm@nabto.com>
2025-07-08 14:14:36 +0200

    Very simple javascript device example

---
+++ examples/very-simple-camera-example/README.md
@@ -0,0 +1,18 @@
+# Very simple camera example.
+
+This very simple camera example shows how to use the
+@nabto/webrtc-signaling-device and the @nabto/webrtc-signaling-util packages to
+create a very simple HTML application which can stream the local webcam to a
+remote peer.
+
+This simple example can be used together with web application on
+https://nabto.github.io/nabto-webrtc-sdk-js running in client mode.
+
+## How to run
+
+```
+npm install
+npx vite
+```
+
+open http://localhost:5173/
---
+++ examples/very-simple-camera-example/main.js
@@ -0,0 +1,35 @@
+import { createSignalingDevice, SignalingErrorCodes } from '@nabto/webrtc-signaling-device'
+import { createClientMessageTransport, ClientMessageTransportSecurityMode, PerfectNegotiation, DeviceTokenGenerator, createDeviceMessageTransport, DeviceMessageTransportSecurityMode } from '@nabto/webrtc-signaling-util'
+
+const productId = "...";
+const deviceId = "...";
+const sharedSecret = "...";
+const privateKey = `-----BEGIN PRIVATE KEY-----
+...
+-----END PRIVATE KEY-----` // a private key in PEM format.
+
+class RTCConnectionHandler {
+    messageTransport;
+    constructor(signalingDevice, channel) {
+        this.messageTransport = createDeviceMessageTransport(signalingDevice, channel, { securityMode: DeviceMessageTransportSecurityMode.SHARED_SECRET, sharedSecretCallback: async (keyId) => { return sharedSecret; } });
+        this.messageTransport.on("setupdone", async (iceServers) => {
+            const pc = new RTCPeerConnection({ iceServers: iceServers });
+            new PerfectNegotiation(pc, this.messageTransport);
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+            stream.getTracks().forEach((track) => {
+                pc.addTrack(track);
+            })
+        })
+    }
+}
+
+function createDevice() {
+    const tokenGenerator = new DeviceTokenGenerator(productId, deviceId, privateKey);
+    const signalingDevice = createSignalingDevice({ productId: productId, deviceId: deviceId, tokenGenerator: () => { return tokenGenerator.generateToken() } });
+    signalingDevice.onNewSignalingChannel = (channel, authorized) => {
+        new RTCConnectionHandler(signalingDevice, channel);
+    }
+    signalingDevice.start();
+}
+
+createDevice();
